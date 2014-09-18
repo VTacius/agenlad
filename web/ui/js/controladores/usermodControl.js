12884d4db84f108e04cac1afad59114ac2bcaa9d
Alexander Ortiz <vtacius@gmail.com>
2014-09-18 14:10:50 -0600

    El elemento switch parecía estar teniendo problemas

--- web/ui/js/controladores/usermodControl.js
+++ web/ui/js/controladores/usermodControl.js
@@ -1,13 +1,21 @@
-$('.btn-toggle').click(function() {
+$('.btn-toggle').click(function(e) {
     $(this).children('.btn').toggleClass('active');  
     $(this).children('.btn').toggleClass('btn-primary');
+    e.stopPropagation(); 
+    e.preventDefault();
 });
 
 $("#enviar").click(function(e){
     console.log($("#buzonstatusBtn").children(".active").text());
     console.log($("#cuentastatusBtn").children(".active").text());
-//    e.stopPropagation(); 
-//    e.preventDefault();
+    e.stopPropagation(); 
+    e.preventDefault();
 });
+
+//$(".btn-sm").click(function(e){
+//    e.stopPropagation(); 
+//    e.preventDefault();    
+//});
+
 
 
